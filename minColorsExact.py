56251a75664c5dfd99e53d5e4d07e521cdef5212
dorvallg <dorvallg@dukes.jmu.edu>
2022-04-25 12:23:58 -0400

    All but NP check

--- minColorsExact.py
+++ minColorsExact.py
@@ -1,14 +1,35 @@
+# Lauren Dorval 
+# Min Colored Graph Exact Code
 
-def checkNP():
+import itertools as it
+
+def checkNP(colors, graph, verts):
     pass
 
-def generatePossible():
-    pass
+def generatePossible(colors, graph, verts):
+    clauses = list(it.product(colors, repeat=len(graph)))
+    is_NP = False
+    for x in range(len(clauses)):
+        is_NP = checkNP(graph, clauses[x], verts)
+        if is_NP:
+            break
 
 def main():
     i = int(input())
+    verticies = {}
+    v = []
     for _ in range(i):
-        pass
+        temp = input().split()
+        verticies[temp[0]] = temp[1:]
+        v.append(temp[0])
+    numColors = 1
+    solved = False
+    while not solved and numColors < len(verticies):
+        colors = []
+        for x in range(numColors):
+            colors.append(x)
+        solved = generatePossible(colors, verticies, v)
+        numColors += 1
 
 if __name__ == "__main__":
     main()
